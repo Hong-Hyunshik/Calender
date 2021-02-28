df8c6520d2df93d08258d1a61c49e72e5e83fb00
Hong-Hyunshik <76999417+Hong-Hyunshik@users.noreply.github.com>
2021-02-28 20:35:13 +0900

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -2,13 +2,12 @@ const table=document.querySelector('table');
 
 function winopen(){
   const ret=window.open('schedule.html',"scheduler","left=50,top=50,width=800,height=600");
+  console.log(self.document);
   return ret;
 }
 
 function forTitle(text,id){
-  console.log(text);
   const title=text.document.querySelector('body');
-  console.log(text.document);
   title.innerText=id;
 }
 
