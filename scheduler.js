bb0d02b7e8dc9de087b7fbe357ef734b34be04c6
Hong-Hyunshik <76999417+Hong-Hyunshik@users.noreply.github.com>
2021-02-28 21:01:52 +0900

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,26 +1,6 @@
 const table=document.querySelector('table');
 
-function winopen(){
-  const ret=window.open('schedule.html',"scheduler","left=50,top=50,width=800,height=600");
-  console.log(self.document);
-  return ret;
-}
 
-function forTitle(text,id){
-  const title=text.document.querySelector('body');
-  title.innerText=id;
-}
-
-function clickhandler(event){
-  event.preventDefault();
-  const win=winopen();
-  const id=event.target.id;
-  forTitle(win,id);
-}
-
-function click(){
-  table.addEventListener("click",clickhandler);
-}
 
 function init(){
   click();
